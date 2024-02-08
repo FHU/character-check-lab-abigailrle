1d5e0b85efb69837f6062ccc060830a1bbe35132
Abigail <abigail.leland@students.fhu.edu>
2024-02-08 12:37:34 -0600

    Update main.py
    test autograder 2 LR

--- main.py
+++ main.py
@@ -16,4 +16,4 @@ if __name__ == '__main__':
     print(check_character('happy birthday', 5))
     print(check_character('happy birthday 2 you', 15))
     print(check_character('happy birthday!', 14))
-   #Test autograder LR
+   #Test autograder LR 2
